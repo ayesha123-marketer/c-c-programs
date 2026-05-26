5125c4b1d799fbcb885e779c1a65133d0fa45bc2
ayesha123-marketer <marketingera567@gmail.com>
2026-05-26 10:53:47 +0500

    tic tac toe design

---
+++ tic tac toe design.cpp
@@ -0,0 +1,16 @@
+// TIC TAC TOE DESIGN
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << " TIC TAC TOE ";
+
+    cout << "\n\n X | O | X ";
+    cout << "\n-----------";
+    cout << "\n O | X | O ";
+    cout << "\n-----------";
+    cout << "\n X | O | X ";
+
+    return 0;
+}
